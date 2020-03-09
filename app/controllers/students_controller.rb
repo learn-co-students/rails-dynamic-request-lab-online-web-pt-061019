e7429ab3432e31c29a4af67d63bf817fd0b17611
Oscar Shoudt <shoudto@gmail.com>
2020-03-09 13:10:07 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,9 @@
-class StudentsController < ApplicationController
+class StudentsController < ApplicationController # create a student controller
   def index
-    @students = Student.all
+    @students = Student.all # create a instance variable to present all students
   end
 
   def show 
-    @student = Student.find_by(params[:id])
+    @student = Student.find_by(params[:id]) # create an instance variable to find the id for a particular student
   end 
 end
